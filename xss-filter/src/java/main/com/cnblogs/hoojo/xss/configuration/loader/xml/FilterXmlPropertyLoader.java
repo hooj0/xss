b8b9020ca0295a130c7e68e8b7142cf38a3e0601
hoojo <hoojo@qq.com>
2018-02-01 12:45:55 +0800

    :sparkles: :bento: :recycle:  Changed 排除过滤xml 配置文件加载器

--- xss-filter/src/java/main/com/cnblogs/hoojo/xss/configuration/loader/xml/FilterXmlPropertyLoader.java
+++ xss-filter/src/java/main/com/cnblogs/hoojo/xss/configuration/loader/xml/FilterXmlPropertyLoader.java
@@ -24,7 +24,6 @@ import com.cnblogs.hoojo.xss.configuration.loader.AbstractPrioritizedPropertyLoa
 
 /**
  * 排除过滤xml 配置文件加载器
- * 
  * @author hoojo
  * @createDate 2018年1月12日 上午10:51:21
  * @file FilterXmlPropertyLoader.java
